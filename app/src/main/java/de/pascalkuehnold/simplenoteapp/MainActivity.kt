f844317d7e0caf3012158fd53800da1e905a7edb
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-16 19:22:25 +0200

    added composable function call to MainActivity.kt

--- app/src/main/java/de/pascalkuehnold/simplenoteapp/MainActivity.kt
+++ app/src/main/java/de/pascalkuehnold/simplenoteapp/MainActivity.kt
@@ -8,19 +8,45 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dagger.hilt.android.AndroidEntryPoint
+import de.pascalkuehnold.simplenoteapp.data.NotesDataSource
+import de.pascalkuehnold.simplenoteapp.model.Note
+import de.pascalkuehnold.simplenoteapp.screens.NoteScreen
+import de.pascalkuehnold.simplenoteapp.screens.NoteViewModel
 import de.pascalkuehnold.simplenoteapp.ui.theme.SimpleNoteAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SimpleNoteAppTheme {
                 // A surface container using the 'background' color from the theme
-
+                Surface(color = MaterialTheme.colors.background) {
+                    NotesApp()
+                }
             }
         }
     }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes()
+
+    NoteScreen(
+        notes = notesList,
+        onAddNote = {
+            noteViewModel.addNote(it)
+        },
+        onRemoveNote = {
+            noteViewModel.removeNote(it)
+        }
+    )
 }
 

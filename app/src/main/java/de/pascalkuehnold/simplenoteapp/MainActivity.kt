29d2fada1bfec099cf9679c8bd83706f28fec2f9
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-16 22:33:30 +0200

    changed the method of getting NoteList Data

--- app/src/main/java/de/pascalkuehnold/simplenoteapp/MainActivity.kt
+++ app/src/main/java/de/pascalkuehnold/simplenoteapp/MainActivity.kt
@@ -3,19 +3,12 @@ package de.pascalkuehnold.simplenoteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.collectAsState
 import dagger.hilt.android.AndroidEntryPoint
-import de.pascalkuehnold.simplenoteapp.data.NotesDataSource
-import de.pascalkuehnold.simplenoteapp.model.Note
 import de.pascalkuehnold.simplenoteapp.screens.NoteScreen
 import de.pascalkuehnold.simplenoteapp.screens.NoteViewModel
 import de.pascalkuehnold.simplenoteapp.ui.theme.SimpleNoteAppTheme
@@ -28,7 +21,10 @@ class MainActivity : ComponentActivity() {
             SimpleNoteAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    NotesApp()
+                    //val noteViewModel = viewModel<NoteViewModel>()
+                    val noteViewModel: NoteViewModel by viewModels()
+
+                    NotesApp(noteViewModel)
                 }
             }
         }
@@ -36,8 +32,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
-    val notesList = noteViewModel.getAllNotes()
+fun NotesApp(noteViewModel: NoteViewModel) {
+    val notesList = noteViewModel.noteList.collectAsState().value
 
     NoteScreen(
         notes = notesList,

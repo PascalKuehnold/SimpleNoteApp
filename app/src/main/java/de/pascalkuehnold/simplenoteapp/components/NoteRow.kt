9332ca1546c9eca8d6e8a1e5a09770fa1cd6e051
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-16 19:20:01 +0200

    added a NoteRow for displaying note details

---
+++ app/src/main/java/de/pascalkuehnold/simplenoteapp/components/NoteRow.kt
@@ -0,0 +1,63 @@
+package de.pascalkuehnold.simplenoteapp.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import de.pascalkuehnold.simplenoteapp.model.Note
+import java.time.format.DateTimeFormatter
+
+
+/**
+ * Created by Pascal Kühnold on 7/15/2022.
+ */
+@Composable
+fun NoteRow(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onNoteClicked: (Note) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
+            .fillMaxWidth(),
+        color = Color(0xFFDFE6EB),
+        elevation = 6.dp
+    ) {
+        Column(
+            modifier = modifier
+                .clickable {
+                    onNoteClicked(note)
+                }
+                .padding(horizontal = 14.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Text(
+                text = note.description,
+                style = MaterialTheme.typography.subtitle1
+            )
+
+            Text(
+                text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE, d MMM")),
+                style = MaterialTheme.typography.caption
+            )
+
+        }
+
+    }
+}

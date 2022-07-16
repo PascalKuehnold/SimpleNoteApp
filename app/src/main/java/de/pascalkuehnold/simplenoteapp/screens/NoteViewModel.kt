b84121cf92fccf3d44bf24247de2ddee15f96dfd
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-16 22:39:20 +0200

    changed the NoteViewModel to communicate with the NoteRepository

--- app/src/main/java/de/pascalkuehnold/simplenoteapp/screens/NoteViewModel.kt
+++ app/src/main/java/de/pascalkuehnold/simplenoteapp/screens/NoteViewModel.kt
@@ -1,31 +1,51 @@
 package de.pascalkuehnold.simplenoteapp.screens
 
-import androidx.compose.runtime.mutableStateListOf
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import de.pascalkuehnold.simplenoteapp.data.NotesDataSource
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import de.pascalkuehnold.simplenoteapp.model.Note
+import de.pascalkuehnold.simplenoteapp.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 /**
  * Created by Pascal Kühnold on 7/15/2022.
  */
-class NoteViewModel: ViewModel() {
 
-    private var noteList = mutableStateListOf<Note>()
+@HiltViewModel
+class NoteViewModel @Inject constructor(private val repository: NoteRepository): ViewModel() {
+
+    private val _noteList = MutableStateFlow<List<Note>>(emptyList())
+    val noteList = _noteList.asStateFlow()
 
     init {
-        noteList.addAll(NotesDataSource().loadNotes())
+        viewModelScope.launch(Dispatchers.IO) {
+            repository
+                .getAllNotes()
+                .collect{ listOfNotes ->
+                    if(listOfNotes.isNullOrEmpty()){
+                        Log.d("Empty", ": Empty List")
+                    }
+                    _noteList.value = listOfNotes
+                }
+        }
     }
 
-    fun addNote(note: Note){
-        noteList.add(note)
+    fun addNote(note: Note) = viewModelScope.launch {
+        repository.addNote(note)
     }
 
-    fun removeNote(note: Note){
-        noteList.remove(note)
+    fun updateNote(note: Note) = viewModelScope.launch {
+        repository.updateNote(note)
     }
 
-    fun getAllNotes(): List<Note>{
-        return noteList
+    fun removeNote(note: Note) = viewModelScope.launch {
+        repository.deleteNote(note)
     }
+
 }

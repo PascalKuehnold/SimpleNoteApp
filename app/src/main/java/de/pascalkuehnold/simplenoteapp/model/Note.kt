b76d7711035c5445b054c3fa612be8abf8a4a431
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-16 22:34:22 +0200

    changed the Note Data class to be an entity for room

--- app/src/main/java/de/pascalkuehnold/simplenoteapp/model/Note.kt
+++ app/src/main/java/de/pascalkuehnold/simplenoteapp/model/Note.kt
@@ -1,15 +1,27 @@
 package de.pascalkuehnold.simplenoteapp.model
 
-import java.time.LocalDateTime
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.util.*
 
 
 /**
  * Created by Pascal Kühnold on 7/15/2022.
  */
+@Entity(tableName = "notes_tbl")
 data class Note(
+
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "note_title")
     val title: String,
+
+    @ColumnInfo(name = "note_description")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+
+    @ColumnInfo(name = "note_entry_date")
+    val entryDate: Date = Date.from(Instant.now())
 )

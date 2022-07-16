d756c0f1bfb0423d7f353c70c4dd74b5bded4643
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-16 22:36:54 +0200

    added the NoteRepository

---
+++ app/src/main/java/de/pascalkuehnold/simplenoteapp/repository/NoteRepository.kt
@@ -0,0 +1,23 @@
+package de.pascalkuehnold.simplenoteapp.repository
+
+import de.pascalkuehnold.simplenoteapp.data.NoteDatabaseDao
+import de.pascalkuehnold.simplenoteapp.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+
+/**
+ * Created by Pascal Kühnold on 7/16/2022.
+ */
+
+class NoteRepository @Inject constructor(private val noteDatabaseDao: NoteDatabaseDao){
+    suspend fun addNote(note: Note) = noteDatabaseDao.insert(note = note)
+    suspend fun updateNote(note: Note) = noteDatabaseDao.update(note = note)
+    suspend fun deleteNote(note: Note) = noteDatabaseDao.delete(note = note)
+    suspend fun deleteAllNotes() = noteDatabaseDao.deleteAll()
+    fun getAllNotes(): Flow<List<Note>> = noteDatabaseDao.getNotes().flowOn(Dispatchers.IO).conflate()
+
+}

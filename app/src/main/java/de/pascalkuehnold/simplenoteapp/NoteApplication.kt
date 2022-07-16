53cd4d0e3c349d3b6bd66d40a2dc442b563173a6
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-16 22:34:42 +0200

    added the HiltAndroidApp Class

---
+++ app/src/main/java/de/pascalkuehnold/simplenoteapp/NoteApplication.kt
@@ -0,0 +1,11 @@
+package de.pascalkuehnold.simplenoteapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by Pascal Kühnold on 7/16/2022.
+ */
+@HiltAndroidApp
+class NoteApplication : Application(){}
